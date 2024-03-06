484b595d3e4e3c4aa8d356fbbe8afc9de18e41e7
cobywg <cobywg@gmail.com>
2024-03-06 02:10:18 -0800

    fixed layout of income page

--- js/pov_chart.js
+++ js/pov_chart.js
@@ -1,26 +1,30 @@
 google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['in_poverty', '%'],
-          ['Yes',     40.56],
-          ['No',      59.44]
-        ]);
+google.charts.setOnLoadCallback(drawChart);
 
-        var options = {
-          title: '% Living in Poverty by Census Tract',
-          is3D: true,
-          backgroundColor:'lightblue',
-          titleTextStyle: {
-            color: 'black' 
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Income Level', '%'],
+        ['Less than $25,000', 20],
+        ['$25,000 - $50,000', 30],
+        ['$50,000 - $75,000', 25],
+        ['$75,000 - $100,000', 15],
+        ['More than $100,000', 10]
+    ]);
+
+    var options = {
+        title: '% of Households by Income Level',
+        is3D: true,
+        backgroundColor: 'lightblue',
+        titleTextStyle: {
+            color: 'black'
         },
         legend: {
             textStyle: {
-                color: 'green' 
+                color: 'green'
             }
         }
-        };
+    };
 
-        var chart = new google.visualization.PieChart(document.getElementById('pov_chart'));
-        chart.draw(data, options);
-      }
+    var chart = new google.visualization.PieChart(document.getElementById('pov_chart'));
+    chart.draw(data, options);
+}
